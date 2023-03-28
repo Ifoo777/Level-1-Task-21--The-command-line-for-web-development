22403e48b6f606dde19c7c3172d213b023477b9e
Irfaan Osman <49390798+Ifoo777@users.noreply.github.com>
2023-03-28 14:55:04 +0200

    Add files via upload

---
+++ file_cd.bat
@@ -0,0 +1,9 @@
+:: This is a comment in the Command Line.
+mkdir Food
+mkdir MainCourse
+mkdir Desserts
+cd Food
+mkdir indian , Asian , Exotic
+rmdir indian , asian
+dir
+cmd.exe
---
+++ ifExample.bat
@@ -0,0 +1,6 @@
+:: This is a comment in the Command Line.
+mkdir newFolder
+cmd.exe
+if exist newFolder mkdir ifFolder
+if exist ifFolder (mkdir hyperiondev) 
+else (mkdir reactProjects)
